26335f4c43f0c7015700dc4d8533d565d7168c94
Vitaliy Orlov <orlov0562@gmail.com>
2015-07-06 02:54:43 +0300

    Small changes in bash example
    Small changes in bash example

--- telegram.sh
+++ telegram.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 APIKEY="---insert-your-API-Key-here---"
+
 URL="https://api.telegram.org/bot$APIKEY"
+CHATID_PARSER='$r=json_decode(file_get_contents("php://stdin"), true); echo isset($r["result"][0]["message"]["chat"]["id"])?$r["result"][0]["message"]["chat"]["id"] : "";'
 
 TEXT="Hello world"
 
-CHATID=$(curl -s "$URL/getUpdates?offset=0" | php -r '$r=json_decode(file_get_contents("php://stdin"), true); echo isset($r["result"][0]["message"]["chat"]["id"])?$r["result"][0]["message"]["chat"]["id"] : "";')
+CHATID=$(curl -s "$URL/getUpdates?offset=0" | php -r "$CHATID_PARSER")
 curl -s -d "chat_id=$CHATID&disable_web_page_preview=1&text=$TEXT" "$URL/sendMessage" >/dev/null 2>&1
-
